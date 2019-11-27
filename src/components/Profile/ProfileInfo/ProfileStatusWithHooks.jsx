de67c7a9e4ce3eef7747b67bd9d39b5a5febb550
Mariia Yevdokymova <mayev@smart-it.com>
2019-11-27 15:32:37 +0200

    85

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+const ProfileStatusWithHooks = (props) => {
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status]);
+
+    let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status);
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    }
+
+    const deactivateEditMode = () => {
+        setEditMode(false);
+        props.updateStatus(status);
+    }
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
+    }
+
+    return (<div>
+        {!editMode && <div>
+            <span onDoubleClick={activateEditMode}>{props.status || '-----'}</span>
+        </div>}
+        {editMode &&
+            <div>
+                <input autoFocus={true}
+                    onChange={onStatusChange} onBlur={deactivateEditMode} value={status}
+                />
+            </div>}
+    </div>)
+
+}
+
+export default ProfileStatusWithHooks
