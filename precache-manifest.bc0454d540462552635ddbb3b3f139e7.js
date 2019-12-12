1f50b69f52e416f04a8d2ba636d5f5862e4d419c
Mariia Yevdokymova <mayev@smart-it.com>
2019-12-12 16:15:21 +0200

    Updates

---
+++ precache-manifest.bc0454d540462552635ddbb3b3f139e7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4761338779ab30f458f5d507d437eae2",
+    "url": "/it-kamasutra/index.html"
+  },
+  {
+    "revision": "6ed3960c64eede30d66a",
+    "url": "/it-kamasutra/static/css/3.42c66d8a.chunk.css"
+  },
+  {
+    "revision": "bbf948ee04ea71721f1e",
+    "url": "/it-kamasutra/static/css/4.dc88aea3.chunk.css"
+  },
+  {
+    "revision": "8af1e08c2ee491a511dd",
+    "url": "/it-kamasutra/static/css/main.cb39f9bf.chunk.css"
+  },
+  {
+    "revision": "38d605cf8167ffe659c5",
+    "url": "/it-kamasutra/static/js/2.5464edc1.chunk.js"
+  },
+  {
+    "revision": "6ed3960c64eede30d66a",
+    "url": "/it-kamasutra/static/js/3.91063889.chunk.js"
+  },
+  {
+    "revision": "bbf948ee04ea71721f1e",
+    "url": "/it-kamasutra/static/js/4.c897c0be.chunk.js"
+  },
+  {
+    "revision": "8af1e08c2ee491a511dd",
+    "url": "/it-kamasutra/static/js/main.56021f4b.chunk.js"
+  },
+  {
+    "revision": "8b14570053a8105d4f85",
+    "url": "/it-kamasutra/static/js/runtime~main.10f2fa12.js"
+  },
+  {
+    "revision": "eded90f3c62d3fa459c18dee89b98fad",
+    "url": "/it-kamasutra/static/media/preloader.eded90f3.svg"
+  },
+  {
+    "revision": "471a1ad342659289433e05a611d206f8",
+    "url": "/it-kamasutra/static/media/user.471a1ad3.png"
+  }
+]);
