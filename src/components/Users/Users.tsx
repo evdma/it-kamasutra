809f4cf27bfef0aef0390b9454496bd418285620
Mariia Yevdokymova <mayev@smart-it.com>
2020-11-24 14:54:07 +0200

    16

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,11 +1,15 @@
 import React, { FC, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import * as queryString from 'querystring';
 import Paginator from '../common/Paginator/Paginator';
 import { actions, FilterType, requestUsers, follow, unfollow } from '../../redux/users-reducer';
 import { getCurrentPage, getFollowingInProgress, getPageSize, getTotalUsersCount, getUsers, getUsersFilter } from '../../redux/users-selectors';
 import UsersSearchForm from './UsersSearchForm';
 import User from './User';
-import styles from "./users.module.css";
+import styles from './users.module.css';
+
+type QueryParamsType = { term?: string; page?: string; friend?: string };
 
 export const Users: FC = () => {
     const users = useSelector(getUsers);
@@ -16,6 +20,7 @@ export const Users: FC = () => {
     const followingInProgress = useSelector(getFollowingInProgress);
 
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const onPageChanged = (pageNumber: number) => {
         dispatch(actions.setCurrentPage(pageNumber));
@@ -33,6 +38,44 @@ export const Users: FC = () => {
     const unfollowUser = (userId: number) => {
         dispatch(unfollow(userId));
     };
+
+    useEffect(() => {
+        const parsed = queryString.parse(history.location.search.substr(1)) as QueryParamsType;
+
+        let actualPage = currentPage;
+        let actualFilter = filter;
+
+        if (!!parsed.page) actualPage = Number(parsed.page);
+        if (!!parsed.term) actualFilter = { ...actualFilter, term: parsed.term as string };
+        if (!!parsed.friend) {
+            switch (parsed.friend) {
+                case 'null':
+                    actualFilter = { ...actualFilter, friend: null };
+                    break;
+                case 'true':
+                    actualFilter = { ...actualFilter, friend: true };
+                    break;
+                case 'false':
+                    actualFilter = { ...actualFilter, friend: false };
+                    break;
+            }
+        }
+
+        dispatch(requestUsers(actualPage, pageSize, actualFilter));
+    }, []);
+
+
+    useEffect(() => {
+        const query: QueryParamsType = {};
+        if (!!filter.term) query.term = filter.term;
+        if (filter.friend !== null) query.friend = String(filter.friend);
+        if (currentPage !== 1) query.page = String(currentPage);
+
+        history.push({
+            pathname: '/users',
+            search: queryString.stringify(query)
+        });
+    }, [filter, currentPage]);
 
     useEffect(() => {
         dispatch(requestUsers(currentPage, pageSize, filter));
--- src/components/Users/UsersSearchForm.tsx
+++ src/components/Users/UsersSearchForm.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from 'react';
+import { useSelector } from 'react-redux';
 import { Formik, Form, Field } from 'formik';
 import { FilterType } from '../../redux/users-reducer';
+import { getUsersFilter } from '../../redux/users-selectors';
 
 const usersSearchFormValidate = (values: any) => {
     const errors = {};
     return errors;
 }
 
+type FriendFormType = "true" | "false" | "null";
+
 type FormType = {
     term: string;
-    friend: "true" | "false" | "null";
+    friend: FriendFormType;
 }
 
 type PropsType = {
@@ -17,6 +21,7 @@ type PropsType = {
 }
 
 const UsersSearchForm: React.FC<PropsType> = React.memo((props) => {
+    const filter = useSelector(getUsersFilter);
 
     const submit = (values: FormType, { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }) => {
         const filter: FilterType = {
@@ -29,7 +34,8 @@ const UsersSearchForm: React.FC<PropsType> = React.memo((props) => {
 
     return <div>
         <Formik
-            initialValues={{ term: '', friend: "null" }}
+            enableReinitialize
+            initialValues={{ term: filter.term, friend: String(filter.friend) as FriendFormType }}
             validate={usersSearchFormValidate}
             onSubmit={submit}
         >
